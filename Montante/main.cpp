f2933a7d577d183bf30491f38a09fa92431593ff
Melissa Treviño <mely.trevic@gmail.com>
2016-03-03 19:42:05 -0600

    Método Montante
    Programa que obtiene la solución de un SEL

--- Montante/main.cpp
+++ Montante/main.cpp
@@ -7,9 +7,102 @@
 //
 
 #include <iostream>
+#include <cmath>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int iCoeficientes[10][10];
+int iMIdentidad[10][10];
+int iMAumentada[20][20];
+
+// Función que obtiene el determinante, lo cual hace con programación dinámica.
+int determinante (int iA, int iB, int iC, int iD)
+{
+    return (iA * iB) - (iC * iD);
+}
+
+// Función que crea una matriz identidad según el tamño dado.
+void crearMIdentidad (int iTam)
+{
+    for (int iReng = 0; iReng < iTam; iReng++)
+    {
+        for (int iCol = 0; iCol < iTam; iCol++)
+        {
+            if (iCol == iReng)
+                iMIdentidad[iReng][iCol] = 1;
+            else
+                iMIdentidad[iReng][iCol] = 0;
+        }
+    }
+}
+
+// Función para desplegar una matriz de cualquier tamaño.
+void mostrarMatriz (int iTamR, int iTamC)
+{
+    for (int iReng = 0; iReng < iTamR; iReng++)
+    {
+        for (int iCol = 0; iCol < iTamC; iCol++)
+        {
+            cout << iMAumentada[iReng][iCol] << "\t";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void creaMatrizAumentada(int iTam)
+{
+    // Primero llena la matriz de coeficientes la parte izquierda.
+    for (int iReng = 0; iReng < iTam; iReng++)
+    {
+        for (int iCol = 0; iCol < iTam; iCol++)
+            iMAumentada[iReng][iCol] = iCoeficientes[iReng][iCol];
+    }
+    
+    // Despúes crear la matriz identidad.
+    crearMIdentidad(iTam);
+    
+    // Por último, agregar esa matriz identidad al lado izquierdo de la matriz aumentada.
+    for (int iReng = 0; iReng < iTam; iReng++)
+    {
+        for (int iCol = iTam; iCol < iTam * 2; iCol++)
+            iMAumentada[iReng][iCol] = iMIdentidad[iReng][iCol - iTam];
+    }
+    
+    mostrarMatriz(iTam, iTam * 2);
+}
+
+int main() {
+    
+    int iEcuaciones, iIncognitas;
+    cout << "   MÉTODO MONTANTE" << endl;
+    cout << "Ingrese el número de ecuaciones: ";
+    cin >> iEcuaciones;
+    cout << "Ingrese el número de incógnitas: ";
+    cin >> iIncognitas;
+    
+    // Validación de que es una matriz cuadrada.
+    while (iEcuaciones != iIncognitas)
+    {
+        cout << "El número de ecuaciones-incógnitas no coinciden." << endl;
+        cout << "Ingrese el número de ecuaciones: ";
+        cin >> iEcuaciones;
+        cout << "Ingrese el número de incógnitas: ";
+        cin >> iIncognitas;
+    }
+    
+    // Ingreso de coeficientes.
+    cout << "Por favor ingrese" << endl;
+    for (int iReng = 0; iReng < iEcuaciones; iReng++)
+    {
+        for (int iCol = 0; iCol < iIncognitas; iCol++)
+        {
+            cout << "El coeficiente #"<< iCol + 1 <<" de la ecuación " << iReng + 1 << ": ";
+            cin >> iCoeficientes[iReng][iCol];
+        }
+    }
+    
+    // Creo la matriz aumentada donde tiene la matriz identidad.
+    creaMatrizAumentada(iEcuaciones);
+    
     return 0;
 }
