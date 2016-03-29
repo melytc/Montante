f22fd4b4d8e359ebfe49dfbe899d3cb940f4e936
Melissa Treviño <mely.trevic@gmail.com>
2016-03-29 10:08:29 -0600

    Se corrigieron errores

--- Montante/main.cpp
+++ Montante/main.cpp
@@ -2,141 +2,26 @@
 //  main.cpp
 //  Montante
 //
-//  Created by Melissa Trevino on 3/3/16.
+//  Created by Melissa Treviño on 3/3/16.
 //
 //  Collaborators:
 //  Melissa Janet Treviño Caballero     A00816715
 //  María Paula Anastás Benavides       A00817285
-//  Luis Felipe Salazar Valenzuela      A00817158
 //
-//  Copyright © 2016 Melissa Trevino.
-//  All rights reserved.
+//  Copyright © 2016 Melissa Treviño. All rights reserved.
 //
 
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-int iCoeficientes[10][10];
-
-// Función que obtiene el determinante de una matriz de 2 x 2.
-int determinante (int iA, int iB, int iC, int iD)
-{
-    return (iA * iB) - (iC * iD);
-}
-
-// Función para desplegar una matriz de cualquier tamaño.
-void mostrarMatriz (int iTamR, int iTamC)
-{
-    for (int iReng = 0; iReng < iTamR; iReng++)
-    {
-        for (int iCol = 0; iCol < iTamC; iCol++)
-        {
-            cout << iCoeficientes[iReng][iCol] << "\t";
-        }
-        cout << endl;
-    }
-    cout << endl;
-}
-
-// Función que obtiene la matriz aumentada modificada después del método de Montante.
-<<<<<<< Updated upstream
-void MatrizMontante (int iTamR, int iTamC) {
-    int iPivote;
-    for (int iReng = 0; iReng < iTamR; iReng++) {
-        for (int iCol = 0; iCol < iTamC; iCol++) {
-=======
-void MatrizMontante (int iTamR, int iTamC, int iMatAct[iTamR][iTamC])
-{
-    int iPivote= iMatAct[0][0], iPivoteAnt=1;
-    int iPivoteReng, iPivoteCol;
-    int iMatAnt[iTamR][iTamC];
-    int iter=0;
-    
-    
-    for (int iReng = 0; iReng < iTamR; iReng++)
-    {
-        for (int iCol = 0; iCol < iTamC; iCol++)
-        {
->>>>>>> Stashed changes
-            // Para tomar la diagonal.
-            if (iReng == iCol) {
-                iPivote = iCoeficientes[iReng][iCol];
-                // Primero hacer ceros hasta la columna del pivote.
-                for ( int iC = 0; iC < iPivoteCol ; iC++)
-                {
-                    if(iC==iTamR)
-                    {
-                    }
-                    else{
-                        iMatAct[iReng][iCol] = 0;
-                    }
-                }
-                // Copiar en la diagonal principal el pivote hasta la columna del pivote.
-                for (int iC=0; iC < iPivoteCol; iC++)
-                {
-                        if(iReng==iCol)
-                            iMatAct[iReng][iCol]=iPivote;
-                }
-                // Copiar el renglón del pivote igual, menos los ceros de la izquierda ya obtenidos.
-                for(int iC=0; iC < iPivoteReng; iC++)
-                {
-                    if(iC>=iPivoteReng)
-                    iMatAnt[iReng][iC]=iMatAct[iC][iCol];
-                }
-                // Hacer dos funciones: uno para el cuadrante I, y otro para el cuadrante IV.
-                //Cuadrante I
-                for(int iR= iPivoteReng ; iR<iTamR; iR++)
-                {
-                    for( int iC = iPivoteCol ; iC > iTamC ; iC++)
-                    {
-            
-                        iMatAct[iR][iC] = ( ((iMatAnt[iR][iC]*iPivote)-(iMatAnt[iter][iC]* iMatAnt[iR][iter]) ) / iPivoteAnt );
-                        iMatAct[iR][iC] = -1*( iMatAct[iR][iC]);
-                        
-                    }
-                    
-                }
-                    
-                //Cuadrante IV
-                for(int iR= iPivoteReng ; iR>iTamR; iR++)
-                {
-                    for( int iC = iPivoteCol ; iC > iTamC ; iC++)
-                    {
-                        
-                        iMatAct[iR][iC] = ( ((iMatAnt[iR][iC]*iPivote)-(iMatAnt[iter][iC]* iMatAnt[iR][iter]) ) / iPivoteAnt );
-                        
-                    }
-                    
-                }
-            }
-            
-        }
-
-    }
-}
-
-
-// Función que resuelve un SEL dada en una matriz.
-<<<<<<< Updated upstream
-void SolucionSEL (int iTamR, int iTamC) {
-    for (int iReng = iTamR - 1; iTamR >= 0; iTamR--) {
-        for (int iCol = ) {
-            
-        }
-=======
-void SolucionSEL (int iTamR, int iTamC)
-{
-    for (int iReng = iTamR - 1; iTamR >= 0; iTamR--)
-    {
-       
->>>>>>> Stashed changes
-    }
-}
-
-
 int main() {
+    // Declaración de variables.
+    int  iMat2[10][10], iMat[10][10], iPivote, iPivoteAnt = 1, iIter = 0;
+    int iRenglon[10];
+    double dRespuestas[10];
+    bool bStat = true;
     
+    // Ingresamos los valores en la matriz
     int iEcuaciones, iIncognitas;
     cout << "   MÉTODO MONTANTE" << endl;
     cout << "Ingrese el número de ecuaciones: ";
@@ -153,21 +38,115 @@ int main() {
         cin >> iIncognitas;
     }
     
-    // Ingreso de coeficientes.
+    cout << endl;
+    iIncognitas++;
+    
+    // Ingreso de coeficientes y constantes.
     cout << "Por favor ingrese" << endl;
-<<<<<<< Updated upstream
     for (int iReng = 0; iReng < iEcuaciones; iReng++) {
-        for (int iCol = 0; iCol < iIncognitas; iCol++) {
-=======
-    for (int iReng = 0; iReng < iEcuaciones; iReng++)
-    {
-        for (int iCol = 0; iCol < iIncognitas+1; iCol++)
-        {
->>>>>>> Stashed changes
+        for (int iCol = 0; iCol < iIncognitas - 1; iCol++) {
             cout << "El coeficiente #"<< iCol + 1 <<" de la ecuación " << iReng + 1 << ": ";
-            cin >> iCoeficientes[iReng][iCol];
+            cin >> iMat[iReng][iCol];
+        }
+        cout << "La constante de la ecuación " << iReng + 1 <<": ";
+        cin >> iMat[iReng][iIncognitas-1];
+        cout << endl;
+    }
+    
+    int iREN = iEcuaciones;
+    int iCOL = iIncognitas;
+    
+    // Se hace una iteracion por cada renglon de la matriz (cada ecuacion)
+    while(iIter < iREN)
+    {
+        
+        // Sacamos el pivote
+        iPivote = iMat[iIter][iIter];
+        
+        // Mientras el pivote valga 0, intercambiamos renglones
+        while (iPivote == 0)
+        {
+            // Se intercambian renglones
+            for(int iC = 0; iC < iCOL; iC++) {
+                iRenglon[iC] = iMat[iIter + 1][iC];
+                iMat[iIter + 1][iC] = iMat[iIter][iC];
+                iMat[iIter][iC] = iRenglon[iC];
+            }
+            
+            // Volvemos a sacar pivote
+            iPivote = iMat[iIter][iIter];
+            
+            // Si no se encuentra un pivote diferente a 0, solución única no tiene.
+            if(iPivote == 0)
+            {
+                // Aquí checamos si no tiene solución o tiene un número infinito de soluciones.
+                for(int i = 0; i < iEcuaciones; i++)
+                {
+                    double resp;
+                    if (iMat[i][i] == 0)
+                    {
+                        if (iMat[i][i+1] != 0)
+                            break;
+                        cout << "No hay solución." << endl;
+                        return 0;
+                    }
+                    resp = iMat[i][iCOL-1] * 1.0 / iMat[i][i];
+                    dRespuestas[i] = resp;
+                }
+                
+                cout << "Hay número infinito de soluciones" << endl;
+                bStat = false;
+                return 0;
+            }
+        }
+        
+        // Pasamos el renglón de la matriz a una matriz nueva.
+        for(int iC = 0; iC < iCOL; iC++)
+            iMat2[iIter][iC] = iMat[iIter][iC];
+        
+        // Ponemos 0s en lo faltante de la columna del pivote.
+        for(int iR = 0; iR < iREN; iR++)
+        {
+            if(iIter != iR)
+                iMat2[iR][iIter] = 0;
+        }
+        
+        // Sacar el valor de la posición según la matriz anterior.
+        for(int iR = 0; iR < iREN; iR++)
+        {
+            for(int iC = 0; iC < iCOL; iC++)
+            {
+                if(iR != iIter)
+                {
+                    if( iC != iIter)
+                        iMat2[iR][iC] = (((iMat[iR][iC] * iPivote) - (iMat[iIter][iC] * iMat[iR][iIter])) / iPivoteAnt);
+                }
+            }
+        }
+        
+        iIter++;
+        iPivoteAnt = iPivote;
+        
+        // Actualizamos la matriz copiando la auxiliar.
+        for(int iR = 0; iR < iREN; iR++)
+        {
+            for(int iC = 0; iC < iCOL; iC++)
+                iMat[iR][iC] = iMat2[iR][iC];
         }
     }
+    
+    
+    if(bStat)
+    {
+        cout << "Solución única" << endl;
+        for(int i = 0; i < iEcuaciones; i++)
+        {
+            double resp;
+            resp = iMat[i][iCOL-1] * 1.0 / iMat[i][i];
+            cout << "x" << i+1 << " = " << resp << endl;
+        }
+    }
+    
     
     return 0;
 }
